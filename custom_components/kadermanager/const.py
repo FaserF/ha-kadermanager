80f0bc8e770931901b28ab44072a66d5d551bed3
Fabian Seitz <fabian.seitz@pari.com>
2024-06-11 12:38:00 +0200

    fix config flow text not being displayed

--- custom_components/kadermanager/const.py
+++ custom_components/kadermanager/const.py
@@ -2,8 +2,8 @@
 DOMAIN = "kadermanager"
 ATTRIBUTION = "Data provided by kadermanager.de"
 
-CONF_TEAM_NAME = "Team Name"
-CONF_USERNAME = "Username (Mail)"
-CONF_PASSWORD = "Password"
-CONF_UPDATE_INTERVAL = "Update Scan interval"
+CONF_TEAM_NAME = "teamname"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_UPDATE_INTERVAL = "update_interval"
 ATTR_DATA = "data"
